cfcd9a857f30ba213332afe038b7d1d63fc57620
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:28:51 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- data.aws_iam_policy_document.lambda.tf
+++ data.aws_iam_policy_document.lambda.tf
@@ -1,4 +1,6 @@
 data "aws_iam_policy_document" "lambda" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   count = (var.create_with_kms_key == 1 ? 1 : 0) * var.create
 
   source_json = data.aws_iam_policy_document.lambda_basic.0.json
